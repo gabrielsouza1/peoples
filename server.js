9eaa48a7c834bfa6204612d2a77247d85fea6822
Anderson Gabriel de Souza <contatogabrielsouza23@gmail.com>
2019-05-21 00:29:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const app = express();
 
 app.get('/api/customers', (req, res) => {
   const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+   {"id": 1,"name": "My name 1","cpf": "04080757247","phone": "11987654321","email": "myemail1@test.com.br"},
+    {"id": 2,"name": "My name 2","cpf": "77797584192","phone": "11987654321", "email": "myemail2@test.com.br"},
+    {"id": 3,"name": "My name 3","cpf": "45486737688","phone": "11987654321","email": "myemail3@test.com.br"},
+    {"id": 4,"name": "My name 4","cpf": "74668869066","phone": "11987654321","email": "myemail4@test.com.br"},
   ];
 
   res.json(customers);
